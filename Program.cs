0f23266e687f6bb9bd8f3b3a70893232a9032da7
Testcsharplearning <forworktosys@gmail.com>
2021-12-15 18:04:15 +0200

    Rewrite program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Lessons{
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Привет мир!");
+
+        }
+    }
+}
